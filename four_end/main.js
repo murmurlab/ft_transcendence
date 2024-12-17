662b7e6827e4b181ba0874d27325ae0d0740f930
murmur <0aeon0@mail.com>
2024-12-17 10:01:32 +0300

    add main page, add index div, login div

---
+++ four_end/main.js
@@ -0,0 +1,36 @@
+const	mode = document.getElementById("mode")
+const	play = document.getElementById("play1")
+
+const	header = document.getElementById("xxl-header")
+const	account = document.getElementById("account")
+const	game = document.getElementById("game")
+const	index = document.getElementById("index")
+const	present = document.getElementById("present")
+
+account.style.display = "none"
+
+play.addEventListener("click", e=>{
+
+	if (account.style.display == "none") {
+		account.style.display = "block"
+		header.style.position = "sticky"
+		header.style.top = "1vmax"
+		present.style.filter = "blur(0.25vmax)"
+	} else {
+		account.style.display = "none"
+		header.style.position = "relative"
+		header.style.top = 0
+		present.style.filter = "blur(0)"
+	}
+	
+})
+mode.addEventListener("change", e=>{
+	console.log(e);
+	if (e.target.checked) {
+		game.style.display = "block"
+		index.style.display = "none"
+	} else {
+		game.style.display = "none"
+		index.style.display = "block"
+	}
+})
